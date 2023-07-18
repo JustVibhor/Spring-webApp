0eae492cd0e05b18b3a1ca9896444cc40ffb46fa
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-19 00:08:14 +0530

    Beans

--- src/main/java/com/vibhor/learnspringframework/App02HelloWorldString.java
+++ src/main/java/com/vibhor/learnspringframework/App02HelloWorldString.java
@@ -8,9 +8,15 @@ public class App02HelloWorldString {
 		
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
-		System.out.println(context.getBean("name"));
+//		System.out.println(context.getBean("name"));
+//		
+//		System.out.println(context.getBean("age"));
+//		
+//		System.out.println(context.getBean("person"));
 		
-		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		
+		
 
 	}
 
--- src/main/java/com/vibhor/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/vibhor/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package com.vibhor.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// does the job of getter, setter, toString and more methods we create when creating a class	
+record Person (String name, int age, Address address) {};
+record Address (String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -14,5 +18,20 @@ public class HelloWorldConfiguration {
 	@Bean
 	public int age() {
 		return 9;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Vibhor", 24, new Address("Dilemantiar", "Aproxima-B"));
+	}
+	
+	@Bean(name = "dusra")
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Sita", "Delhi");
 	}
 }
