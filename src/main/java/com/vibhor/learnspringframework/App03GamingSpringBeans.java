36554996ebf4530410efa85cc074509a6e2d077e
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-19 17:44:55 +0530

    functional prog

--- src/main/java/com/vibhor/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/vibhor/learnspringframework/App03GamingSpringBeans.java
@@ -1,5 +1,7 @@
 package com.vibhor.learnspringframework;
 
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.vibhor.learnspringframework.game.GameRunner;
@@ -11,11 +13,20 @@ public class App03GamingSpringBeans {
 
 	public static void main(String[] args) {
 
-		try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
-
-			context.getBean(GameRunner.class).run();
-		}
+//		try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+//
+//			context.getBean(GameRunner.class).run();
+//		}
+		
+		
+		List<String> numbers;
+		numbers = List.of("Spring", "SpringBoot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "kubernetes");
+		
+		numbers.stream()
+		.filter(number -> number.length() >= 4)
+		.forEach(System.out::println);
 
 	}
+
 
 }
