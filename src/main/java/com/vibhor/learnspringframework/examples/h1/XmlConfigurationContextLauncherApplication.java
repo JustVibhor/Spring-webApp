f16c6b1ebbc01c6b322c9e10f1200f392a990f06
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-23 14:09:19 +0530

    configuring beans using XML

--- src/main/java/com/vibhor/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ src/main/java/com/vibhor/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -1,20 +1,15 @@
 package com.vibhor.learnspringframework.examples.h1;
 
-import java.util.Arrays;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan()
 public class XmlConfigurationContextLauncherApplication {
 
 	public static void main(String[] args) {
 
-		try (var context = new AnnotationConfigApplicationContext(XmlConfigurationContextLauncherApplication.class)) {
-			Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
+		try (var context = new ClassPathXmlApplicationContext("ContextConfiguration.xml")) {
+//			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
 		}
 
 	}
