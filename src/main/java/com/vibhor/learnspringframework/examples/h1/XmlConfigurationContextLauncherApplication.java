3937f04497df06fa92b4ec2c8c7b05c9e9fcae96
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-23 14:10:59 +0530

    configuring package using context.componenscan

--- src/main/java/com/vibhor/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ src/main/java/com/vibhor/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -1,5 +1,7 @@
 package com.vibhor.learnspringframework.examples.h1;
 
+import java.util.Arrays;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class XmlConfigurationContextLauncherApplication {
@@ -7,7 +9,7 @@ public class XmlConfigurationContextLauncherApplication {
 	public static void main(String[] args) {
 
 		try (var context = new ClassPathXmlApplicationContext("ContextConfiguration.xml")) {
-//			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			System.out.println(context.getBean("name"));
 			System.out.println(context.getBean("age"));
 		}
